ece4a0abf6dee970d588fae000fc664f3bf36560
Jacob Momoh <jacobmomoh20@gmail.com>
2020-01-19 13:55:17 +0100

    basic commit 2

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $("#header , .info").ripples({
+        dropRadius: 30,
+        perturbance: 0.6,
+        
+      });
+
+      //magnific popup
+
+      $('.parent-container').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery:{
+            enabled:true
+        }
+        // other options
+      });
+});
+
